f2d44bbcfded8c13dbe1ae50aaf7b23dd6622500
Pentarh Udi <pentarh@gmail.com>
2012-02-14 04:29:38 +0200

    description added, default network id changed

--- src/base58.h
+++ src/base58.h
@@ -259,7 +259,7 @@ class CBitcoinAddress : public CBase58Data
 public:
     bool SetHash160(const uint160& hash160)
     {
-        SetData(fTestNet ? 112 : 53, &hash160, 20);
+        SetData(fTestNet ? 112 : 30, &hash160, 20);
         return true;
     }
 
